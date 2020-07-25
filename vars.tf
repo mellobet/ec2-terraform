5707db3384415c7ca10f7ef8a1550a28a10ecdc2
Mario Llobet <mario.llobet@onapsis.com>
2020-07-25 18:31:51 -0300

    new network

--- vars.tf
+++ vars.tf
@@ -1,11 +1,11 @@
 variable "cidr_vpc" {
   description = "CIDR block for the VPC"
-  default = "10.0.0.0/16"
+  default = "10.1.0.0/16"
 }
 
 variable "cidr_subnet" {
   description = "CIDR block for the subnet"
-  default = "10.0.0.0/24"
+  default = "10.1.0.0/24"
 }
 
 variable "availability_zone" {
