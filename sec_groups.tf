60b650855bfed549b190dc9360c4f3ad7e0fb1d6
Mario Llobet <mario.llobet@onapsis.com>
2020-12-16 10:22:20 -0300

    added more sec groups

---
+++ sec_groups.tf
@@ -0,0 +1,81 @@
+resource "aws_security_group" "allow_ssh" {
+  name   = "allow_ssh"
+  vpc_id = aws_vpc.tf-main-vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    # only from own block.
+    # cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  # -1 = "all"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "allow_web" {
+  name   = "allow_web"
+  vpc_id = aws_vpc.tf-main-vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # -1 = "all"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "allow_jupyternb" {
+  name   = "allow_jupyternb"
+  vpc_id = aws_vpc.tf-main-vpc.id
+
+  ingress {
+    from_port   = 8888
+    to_port     = 8888
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # -1 = "all"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "allow_dango_devsrv" {
+  name   = "allow_dango_devsrv"
+  vpc_id = aws_vpc.tf-main-vpc.id
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # -1 = "all"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
